1abe4469924d9a2c9e95134d18882f8f593faa37
Jonathan Vazquez <yair.vazquez001@gmail.com>
2021-12-29 15:25:50 -0600

    Detalles

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,85 +1,82 @@
 /* Menu de hamburgues */
 
-const navToggle = document.querySelector('.nav-toggle')
-const navMenu = document.querySelector('.nav-menu')
+const navToggle = document.querySelector(".nav-toggle");
+const navMenu = document.querySelector(".nav-menu");
 
-navToggle.addEventListener('click',() => {
-    navMenu.classList.toggle('nav-menu_visible')
-})
+navToggle.addEventListener("click", () => {
+  navMenu.classList.toggle("nav-menu_visible");
+});
 
 /* Ocultar proyectos */
 
-function mostrarIA(){
-    document.getElementById('ia-proyectos').style.display = 'grid';
-    document.getElementById('web-proyectos').style.display = 'none';
-    document.getElementById('escritos-proyectos').style.display = 'none';
-    document.getElementById('dibujos-proyectos').style.display = 'none';
+function mostrarIA() {
+  document.getElementById("ia-proyectos").style.display = "grid";
+  document.getElementById("web-proyectos").style.display = "none";
+  document.getElementById("escritos-proyectos").style.display = "none";
+  document.getElementById("dibujos-proyectos").style.display = "none";
 
-    document.getElementById('ia').style.color = 'var(--rosa)';
-    document.getElementById('web').style.color = 'var(--gris-claro)';
-    document.getElementById('escritos').style.color = 'var(--gris-claro)';
-    document.getElementById('dibujos').style.color = 'var(--gris-claro)';
+  document.getElementById("ia").style.color = "var(--rosa)";
+  document.getElementById("web").style.color = "var(--gris-claro)";
+  document.getElementById("escritos").style.color = "var(--gris-claro)";
+  document.getElementById("dibujos").style.color = "var(--gris-claro)";
 }
 
-function mostrarWeb(){
-    document.getElementById('ia-proyectos').style.display = 'none';
-    document.getElementById('web-proyectos').style.display = 'grid';
-    document.getElementById('escritos-proyectos').style.display = 'none';
-    document.getElementById('dibujos-proyectos').style.display = 'none';
+function mostrarWeb() {
+  document.getElementById("ia-proyectos").style.display = "none";
+  document.getElementById("web-proyectos").style.display = "grid";
+  document.getElementById("escritos-proyectos").style.display = "none";
+  document.getElementById("dibujos-proyectos").style.display = "none";
 
-    document.getElementById('ia').style.color = 'var(--gris-claro)';
-    document.getElementById('web').style.color = 'var(--rosa)';
-    document.getElementById('escritos').style.color = 'var(--gris-claro)';
-    document.getElementById('dibujos').style.color = 'var(--gris-claro)';
+  document.getElementById("ia").style.color = "var(--gris-claro)";
+  document.getElementById("web").style.color = "var(--rosa)";
+  document.getElementById("escritos").style.color = "var(--gris-claro)";
+  document.getElementById("dibujos").style.color = "var(--gris-claro)";
 }
 
-function mostrarEscritos(){
-    document.getElementById('ia-proyectos').style.display = 'none';
-    document.getElementById('web-proyectos').style.display = 'none';
-    document.getElementById('escritos-proyectos').style.display = 'grid';
-    document.getElementById('dibujos-proyectos').style.display = 'none';
+function mostrarEscritos() {
+  document.getElementById("ia-proyectos").style.display = "none";
+  document.getElementById("web-proyectos").style.display = "none";
+  document.getElementById("escritos-proyectos").style.display = "grid";
+  document.getElementById("dibujos-proyectos").style.display = "none";
 
-    document.getElementById('ia').style.color = 'var(--gris-claro)';
-    document.getElementById('web').style.color = 'var(--gris-claro)';
-    document.getElementById('escritos').style.color = 'var(--rosa)';
-    document.getElementById('dibujos').style.color = 'var(--gris-claro)';
+  document.getElementById("ia").style.color = "var(--gris-claro)";
+  document.getElementById("web").style.color = "var(--gris-claro)";
+  document.getElementById("escritos").style.color = "var(--rosa)";
+  document.getElementById("dibujos").style.color = "var(--gris-claro)";
 }
 
-function mostrarDibujos(){
-    document.getElementById('ia-proyectos').style.display = 'none';
-    document.getElementById('web-proyectos').style.display = 'none';
-    document.getElementById('escritos-proyectos').style.display = 'none';
-    document.getElementById('dibujos-proyectos').style.display = 'grid';
+function mostrarDibujos() {
+  document.getElementById("ia-proyectos").style.display = "none";
+  document.getElementById("web-proyectos").style.display = "none";
+  document.getElementById("escritos-proyectos").style.display = "none";
+  document.getElementById("dibujos-proyectos").style.display = "grid";
 
-    document.getElementById('ia').style.color = 'var(--gris-claro)';
-    document.getElementById('web').style.color = 'var(--gris-claro)';
-    document.getElementById('escritos').style.color = 'var(--gris-claro)';
-    document.getElementById('dibujos').style.color = 'var(--rosa)';
+  document.getElementById("ia").style.color = "var(--gris-claro)";
+  document.getElementById("web").style.color = "var(--gris-claro)";
+  document.getElementById("escritos").style.color = "var(--gris-claro)";
+  document.getElementById("dibujos").style.color = "var(--rosa)";
 }
 
 /* Mostrar imagenes */
 
-const images = document.querySelectorAll('.imgP');
-const containerImage = document.querySelector('.container-img');
-const imageC = document.querySelector('.img-show');
+const images = document.querySelectorAll(".imgP");
+const containerImage = document.querySelector(".container-img");
+const imageC = document.querySelector(".img-show");
 
-images.forEach(image =>{
-    image.addEventListener("click",()=>{
-        addImage(image.getAttribute('src'),image.getAttribute('alt'));
-    })
-})
+images.forEach((image) => {
+  image.addEventListener("click", () => {
+    addImage(image.getAttribute("src"), image.getAttribute("alt"));
+  });
+});
 
-const addImage = (srcImage,altImage)=>{
-    containerImage.classList.toggle("move")
-    imageC.classList.toggle("show")
-    imageC.src = srcImage;
-    copy.innerHTML = altImage;
-}
+const addImage = (srcImage, altImage) => {
+  containerImage.classList.toggle("move");
+  imageC.classList.toggle("show");
+  imageC.src = srcImage;
+  copy.innerHTML = altImage;
+};
 
-containerImage.addEventListener("click", ()=>{
-    containerImage.classList.toggle("move")
-    imageC.classList.toggle("show")
-})
-
-
-
+containerImage.addEventListener("click", () => {
+  containerImage.classList.toggle("move");
+  imageC.classList.toggle("show");
+});
